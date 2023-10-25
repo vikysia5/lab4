415694554d021778685f95dc5ee6e65fefab49e6
vikysia5 <145292763+vikysia5@users.noreply.github.com>
2023-10-25 06:28:37 +0000

    main

--- .ccls-cache/@home@runner@lab4/main.cpp
+++ .ccls-cache/@home@runner@lab4/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main()
+{
+ double x, y; 
+ int k = 1;    
+ cout<<"Введiть значення x= ";    cin>> x; 
+ cout<< "\nРезультати:\n";
+ M1:  
+switch ( k )
+  {
+    case 1:  1;
+
+    case 2:  1;
+
+    case 3:   1;
+
+    case 4:   1;
+  }
+ cout<< "y" << k << " = " << y << endl;
+ k++;
+ if (k <= 4) goto M1;
+ return 0;
 }
